127e49cf2b3b3c5a8378b6cf57246b8242236600
abhishekjinxed <abhishekjinxed@gmail.com>
2022-07-11 13:32:09 +0530

    1 file modified

--- src/createMeme.js
+++ src/createMeme.js
@@ -4,7 +4,7 @@ function createMeme(props) {
   return (
     <div>
       <h1>Home{props.name}</h1>
-      <p>hey abhi Homepage bhi body content</p>
+      <p>hey bt abhi Homepage bhi body content</p>
     </div>
   );
 }
